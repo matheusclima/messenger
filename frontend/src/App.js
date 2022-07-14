60a9d8b6b7fab9e3b62fd981d1f1640136ecd054
matheusclima <matheuslima1607@gmail.com>
2022-07-14 17:24:34 -0300

    Criação da estrutura inicial do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,27 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <h1>Header</h1>
+      </header>
+
+      <main>
+
+        <div className="chat-list">
+          <h1>Lista de Chats</h1>  
+        </div>
+        
+        <div className="chat-list__messages">
+          <h1>Conversa</h1>
+        </div>
+     
+      </main>
+
+      <footer className="footer">
+        <h1>Footer</h1>
+      </footer>
     </div>
   );
 }
