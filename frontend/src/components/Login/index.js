180105880fbb6196bc2e5cfe6bea1a2ac97e9863
matheusclima <matheuslima1607@gmail.com>
2022-09-01 03:17:48 -0300

    [FEATURE] Estilização da página de Login

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,49 +1,61 @@
 import React, { useState } from "react"
 import api from "../../services/api"
 import auth from "../../services/auth"
+import icon from "../../img/login.png"
 import styles from "./style.module.css"
 
 function Login() {
 
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
+    const [user, setUser] = useState({
+        username: "",
+        password: ""
+    })
 
     const submitLoginInfo = async (event) => {
         event.preventDefault()
-        let response = await api.sendLogin({username, password})
+        let response = await api.sendLogin({
+            username: user.username, 
+            password: user.password
+        })
         auth.login(response.token)
+    }
+
+    const handleLoginInfo = (e) => {
+        setUser(prevUser => ({...prevUser, [e.target.name]: e.target.value}))
     }
 
     return (
         <main className={styles.main}>
             <section>
                 <form action="" method="POST" onSubmit={submitLoginInfo}>
+                    <img src={icon} alt="Login icon"></img>
+                    <h3>Bem-vindo</h3>
                     <label className="input--field">
-                        <p>Username: </p>
                         <input
                         type="text"
                         name="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
+                        value={user.username}
+                        placeholder=" "
+                        onChange={handleLoginInfo}
                         ></input>
+                        <p>Username</p>
+                        
                     </label>
                     <label className="input--field">
-                        <p>Password: </p>
                         <input 
                         type="password"
                         name="password" 
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        value={user.password}
+                        placeholder=" "
+                        onChange={handleLoginInfo}
                         ></input>
+                        <p>Password</p>
                     </label>
                     <button type="submit" className={styles.login__button}>Login</button>
                     <p>
                         Ainda não é cadastrado? Clique <a href="/">aqui</a>
                     </p>
                 </form>
-            </section>
-
-            <section>
             </section>
         </main>
     )
